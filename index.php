e1df07f2c586e65525f8b362cf45ae31d59fb25a
Hung Tran <nguyentieuhau@gmail.com>
2024-06-25 20:12:23 +0700

    Get contact info, update street address

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use Microsoft\Kiota\Abstractions\ApiException;
 use Microsoft\Graph\GraphServiceClient;
-use Microsoft\Graph\Generated\Users\Item\AppRoleAssignments\AppRoleAssignmentsRequestBuilderGetRequestConfiguration;
+use Microsoft\Graph\Generated\Users\Item\UserItemRequestBuilderGetRequestConfiguration;
+
 use Microsoft\Graph\Core\Tasks\PageIterator;
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -25,6 +26,8 @@ while ($choice != 0) {
     echo '2. Get current name\'s display name' . PHP_EOL;
     echo '3. Change current name\'s display name' . PHP_EOL;
     echo '4. Get current name\'s roles' . PHP_EOL;
+    echo '5. Get current name\'s contact info' . PHP_EOL;
+    echo '6. Change current name\'s street address' . PHP_EOL;
     echo PHP_EOL;
 
     $choice = (int) readline('');
@@ -41,9 +44,14 @@ while ($choice != 0) {
         case 3:
             setUserDisplayName();
             break;
-            break;
         case 4:
             getUserRoles();
+            break;
+        case 5:
+            getUserContactInfo();
+            break;
+        case 6:
+            setUserStreetAddress();
             break;
         default:
             print('Goodbye...' . PHP_EOL);
@@ -126,6 +134,62 @@ function getUserRoles() {
         echo 'Error: ' . $e->getError()->getMessage();
         exit(0);
     }
+}
+
+function getUserContactInfo() {
+    global $graphServiceClient;
+    global $userPrincipalName;
+
+    echo 'Please wait...' . PHP_EOL;
+
+    try {
+        $requestConfiguration = new UserItemRequestBuilderGetRequestConfiguration();
+        $queryParameters = UserItemRequestBuilderGetRequestConfiguration::createQueryParameters();
+        $queryParameters->select = ['streetAddress', 'city', 'state', 'postalCode', 'country', 'businessPhones', 'mobilePhone', 'mail', 'faxNumber', 'imAddresses', 'mailNickname'];
+        $requestConfiguration->queryParameters = $queryParameters;
+
+        /** @var Microsoft\Graph\Generated\Models\User $user */
+        $user = $graphServiceClient->users()->byUserId($userPrincipalName)->get($requestConfiguration)->wait();
+        echo 'Street address: ' . $user->getStreetAddress() . PHP_EOL;
+        echo 'City: ' . $user->getCity() . PHP_EOL;
+        echo 'State or province: ' . $user->getState() . PHP_EOL;
+        echo 'ZIP or postal code: ' . $user->getPostalCode() . PHP_EOL;
+        echo 'Country or region: ' . $user->getCountry() . PHP_EOL;
+        echo 'Business phone: ' . PHP_EOL;
+        print_r($user->getBusinessPhones()) . PHP_EOL;
+        echo 'Mobile phone: ' . $user->getMobilePhone() . PHP_EOL;
+        echo 'Email: ' . $user->getMail() . PHP_EOL;
+        echo 'Fax number: ' . $user->getFaxNumber() . PHP_EOL;
+        echo 'IM addresses: ' . PHP_EOL;
+        print_r($user->getImAddresses());
+        echo 'Mail nickname: ' . $user->getMailNickname() . PHP_EOL;
+
+    } catch (ApiException $e) {
+        echo 'Error: ' . $e->getError()->getMessage();
+        exit(0);
+    }
+}
+
+function setUserStreetAddress() {
+    global $graphServiceClient;
+    global $userPrincipalName;
+
+    echo "Please enter a new street address for $userPrincipalName" . PHP_EOL;
+
+    $newStreetAddress = readline('');
+
+    echo 'Please wait...' . PHP_EOL;
+
+    try {
+        $user = $graphServiceClient->users()->byUserId($userPrincipalName)->get()->wait();
+        $user->setStreetAddress($newStreetAddress);
+        $graphServiceClient->users()->byUserId($userPrincipalName)->patch($user)->wait();
+    } catch (ApiException $e) {
+        echo 'Error: ' . $e->getError()->getMessage();
+        exit(0);
+    }
+
+    echo 'Current user\'s street address is now ' . $user->getDisplayName() . PHP_EOL;
 }
 
 function getGraphServiceClient() {
